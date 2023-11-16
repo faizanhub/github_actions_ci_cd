a70274d8868be396961305e342d3c3264ca74eb3
Faizan <faizii.jay@gmail.com>
2023-11-16 15:50:40 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Github Actions CI/CD'),
+      home: const MyHomePage(title: 'Updated Github Actions CI/CD'),
     );
   }
 }
